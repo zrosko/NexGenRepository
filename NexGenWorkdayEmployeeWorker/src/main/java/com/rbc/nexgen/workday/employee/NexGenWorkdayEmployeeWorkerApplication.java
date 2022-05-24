f14197fb8da6b663e02d9d14ed3faee7b7929366
zdravko rosko <zrosko@gmail.com>
2022-05-24 13:46:43 -0400

    Initial.

---
+++ NexGenWorkdayEmployeeWorker/src/main/java/com/rbc/nexgen/workday/employee/NexGenWorkdayEmployeeWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.rbc.nexgen.workday.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexGenWorkdayEmployeeWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexGenWorkdayEmployeeWorkerApplication.class, args);
+	}
+
+}
---
+++ NexGenWorkdayEmployeeWorker/src/test/java/com/rbc/nexgen/workday/employee/NexGenWorkdayEmployeeWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbc.nexgen.workday.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexGenWorkdayEmployeeWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
