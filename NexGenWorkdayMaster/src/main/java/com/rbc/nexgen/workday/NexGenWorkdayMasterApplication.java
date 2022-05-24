6cf9de6411f120532ddb154d66a158bda18c3627
zdravko rosko <zrosko@gmail.com>
2022-05-24 13:47:12 -0400

    Initial

---
+++ NexGenWorkdayMaster/src/main/java/com/rbc/nexgen/workday/NexGenWorkdayMasterApplication.java
@@ -0,0 +1,13 @@
+package com.rbc.nexgen.workday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexGenWorkdayMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexGenWorkdayMasterApplication.class, args);
+	}
+
+}
---
+++ NexGenWorkdayMaster/src/test/java/com/rbc/nexgen/workday/NexGenWorkdayMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbc.nexgen.workday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexGenWorkdayMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
