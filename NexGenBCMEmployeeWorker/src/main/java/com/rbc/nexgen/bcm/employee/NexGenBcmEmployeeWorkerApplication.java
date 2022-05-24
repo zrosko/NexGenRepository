539ab47c2c3959d58c0c32865c4d9b4008a27bfd
zdravko rosko <zrosko@gmail.com>
2022-05-24 13:49:35 -0400

    Initial.

---
+++ NexGenBCMEmployeeWorker/src/main/java/com/rbc/nexgen/bcm/employee/NexGenBcmEmployeeWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.rbc.nexgen.bcm.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexGenBcmEmployeeWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexGenBcmEmployeeWorkerApplication.class, args);
+	}
+
+}
---
+++ NexGenBCMEmployeeWorker/src/test/java/com/rbc/nexgen/bcm/employee/NexGenBcmEmployeeWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbc.nexgen.bcm.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexGenBcmEmployeeWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
