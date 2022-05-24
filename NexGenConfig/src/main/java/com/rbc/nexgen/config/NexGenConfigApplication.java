7158eb0cfa77503d7e7218c2cfbcf9cd68be73fc
zdravko rosko <zrosko@gmail.com>
2022-05-24 13:44:33 -0400

    Initial.

---
+++ NexGenConfig/src/main/java/com/rbc/nexgen/config/NexGenConfigApplication.java
@@ -0,0 +1,19 @@
+package com.rbc.nexgen.config;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class NexGenConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication(NexGenConfigApplication.class);
+		//application.setBannerMode(Banner.Mode.OFF);
+		application.setBanner(new NexGenBanner());
+		application.run(args);
+	}
+
+}
---
+++ NexGenConfig/src/test/java/com/rbc/nexgen/config/NexGenConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbc.nexgen.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexGenConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
