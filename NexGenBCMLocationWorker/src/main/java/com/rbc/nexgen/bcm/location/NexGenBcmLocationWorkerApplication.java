fea0a2731b8978a8f04cab8f83fe6bb10d46dcf6
zdravko rosko <zrosko@gmail.com>
2022-05-24 13:49:19 -0400

    Initial.

---
+++ NexGenBCMLocationWorker/src/main/java/com/rbc/nexgen/bcm/location/NexGenBcmLocationWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.rbc.nexgen.bcm.location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexGenBcmLocationWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexGenBcmLocationWorkerApplication.class, args);
+	}
+
+}
---
+++ NexGenBCMLocationWorker/src/test/java/com/rbc/nexgen/bcm/location/NexGenBcmLocationWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbc.nexgen.bcm.location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexGenBcmLocationWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
