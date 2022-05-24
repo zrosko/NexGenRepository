5bff5db176d9bb865eec7c648b57663c10db265e
zdravko rosko <zrosko@gmail.com>
2022-05-24 13:50:16 -0400

    Initial.

---
+++ NexGenArcherMaster/src/main/java/com/rbc/nexgen/archer/NexGenArcherMasterApplication.java
@@ -0,0 +1,13 @@
+package com.rbc.nexgen.archer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexGenArcherMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexGenArcherMasterApplication.class, args);
+	}
+
+}
---
+++ NexGenArcherMaster/src/test/java/com/rbc/nexgen/archer/NexGenArcherMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbc.nexgen.archer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexGenArcherMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
