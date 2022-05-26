55a381f49ab03a762c34abdaca996df337083820
zdravko rosko <zrosko@gmail.com>
2022-05-25 21:58:05 -0400

    cleanup

---
+++ NexGenHelloWorld/src/main/java/com/rbc/nexgen/helloworld/restclient/OtherServiceClient.java
@@ -0,0 +1,17 @@
+package com.rbc.nexgen.helloworld.restclient;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.rbc.nexgen.helloworld.model.User;
+//TODO Change this direct Microservice call using Eureka to call Gateway
+@FeignClient("gateway-service")
+//@FeignClient("otherservice")
+public interface OtherServiceClient {
+	
+	@RequestMapping(method = RequestMethod.POST, value = "users", consumes = "application/json")
+	List<User> getUsers();
+}
---
+++ NexGenHelloWorld/src/test/java/com/rbc/nexgen/helloworld/NexGenHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbc.nexgen.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexGenHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
